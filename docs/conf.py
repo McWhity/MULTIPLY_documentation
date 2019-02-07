c827e251bffe6318b397a91b19f93a2e2a14fe68
McWhity <weiss.thomas@lmu.de>
2019-02-07 12:37:27 +0100

    debug doc

--- docs/conf.py
+++ docs/conf.py
@@ -15,7 +15,7 @@ import mock
 
 MOCK_MODULES = ['bs4', 'html5lib', 'multiply_core.util', 'multiply_core.observations', 'multiply_core',
                 'shapely.geometry', 'shapely.geometry.base', 'shapely.ops', 'shapely.wkt', 'yaml', 'request', 'gdal',
-                'osr', 'sentinelhub', 'h5py', 'netCDF4']
+                'osr', 'sentinelhub', 'h5py']
 
 for mod_name in MOCK_MODULES:
     sys.modules[mod_name] = mock.Mock()
