396b25a2b5e702e7b977f8caa29a20b9a19b5e63
McWhity <weiss.thomas@lmu.de>
2019-02-07 16:17:06 +0100

    hm

--- docs/atmospheric_correction/SIAC/__init__.py
+++ docs/atmospheric_correction/SIAC/__init__.py
@@ -3,4 +3,4 @@ from .the_correction import atmospheric_correction
 from .atmo_solver import solving_atmo_paras
 from .SIAC_S2 import SIAC_S2
 from .SIAC_L8 import SIAC_L8
-
+from .kernels import kernels
